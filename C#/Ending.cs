d01631a0c22a6c8f7cc33d8933d4facf599ee24d
min-vely <81990267+min-vely@users.noreply.github.com>
2023-01-31 22:00:23 +0900

    Add files via upload

---
+++ C#/Ending.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ending : MonoBehaviour
+{
+ 
+    public void BacktoMain ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
